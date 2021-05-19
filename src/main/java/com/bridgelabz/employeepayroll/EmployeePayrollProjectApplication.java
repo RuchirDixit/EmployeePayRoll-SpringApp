7f29d8936f8682c9f733d5f8e0b6dae01c103582
RuchirDixit <ruchirtd96@gmail.com>
2021-05-19 20:19:14 +0530

    [Ruchir]Added: Loggers

--- src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollProjectApplication.java
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollProjectApplication.java
@@ -1,12 +1,16 @@
 package com.bridgelabz.employeepayroll;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmployeePayrollProjectApplication {
 
+	public static final Logger logger = LoggerFactory.getLogger(EmployeePayrollProjectApplication.class);
 	public static void main(String[] args) {
+		logger.debug("Inside Employee Payroll Project main....");
 		SpringApplication.run(EmployeePayrollProjectApplication.class, args);
 	}
 
