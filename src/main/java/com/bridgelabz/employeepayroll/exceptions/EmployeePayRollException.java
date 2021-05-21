f7c3d00ef88f6458f5709e8bd2bfeea4f48a92b0
RuchirDixit <ruchirtd96@gmail.com>
2021-05-21 20:57:12 +0530

    [Ruchir]Added: Exception handler class

---
+++ src/main/java/com/bridgelabz/employeepayroll/exceptions/EmployeePayRollException.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.employeepayroll.exceptions;
+
+public class EmployeePayRollException extends RuntimeException {
+
+	public EmployeePayRollException(String message) {
+		super(message);
+	}
+}
