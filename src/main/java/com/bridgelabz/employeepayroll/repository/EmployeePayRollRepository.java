7169ec21e77f41feb04ea5bd3f2e4e2f7c89dcc6
RuchirDixit <ruchirtd96@gmail.com>
2021-05-23 13:29:03 +0530

    [Ruchir]Added: Custom query to get emp with department

--- src/main/java/com/bridgelabz/employeepayroll/repository/EmployeePayRollRepository.java
+++ src/main/java/com/bridgelabz/employeepayroll/repository/EmployeePayRollRepository.java
@@ -1,9 +1,14 @@
 package com.bridgelabz.employeepayroll.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.bridgelabz.employeepayroll.model.EmployeePayrollData;
 
 public interface EmployeePayRollRepository extends JpaRepository<EmployeePayrollData, Integer> {
+	
+	@Query(value = "select * from employee_payroll,employee_department where employee_id=id and department=:department",nativeQuery = true)
+	public List<EmployeePayrollData> getEmployeeByDepartment(String department);
 }
